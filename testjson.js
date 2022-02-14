0043b6b51fba57be1b8cd1ccdf5ec5c0260cc390
wexx <wex.stu.ouc.edu.cn>
2022-02-14 19:15:04 +0800

    Initial commit

--- testjson.js
+++ testjson.js
@@ -1,2 +1,60 @@
+/*
+ * @Author: your name
+ * @Date: 2022-02-14 10:16:48
+ * @LastEditTime: 2022-02-14 19:14:03
+ * @LastEditors: your name
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: \noded:\学习\github.io\testjson.js
+ */
 export default {
-    "sites":[{"path":"钢琴 - 副本 (2)","name":"菜鸟教程","url":"www.pixiv.com","img":"../0.png"},{"path":"钢琴 - 副本 (3)","name":"菜鸟教程","url":"www.pixiv.com","img":"../0.png"},{"path":"钢琴 - 副本 (4)","name":"菜鸟教程","url":"www.pixiv.com","img":"../0.png"},{"path":"钢琴 - 副本 (5)","name":"菜鸟教程","url":"www.pixiv.com","img":"../0.png"},{"path":"钢琴 - 副本 (6)","name":"菜鸟教程","url":"www.pixiv.com","img":"../0.png"},{"path":"钢琴 - 副本 (7)","name":"菜鸟教程","url":"www.pixiv.com","img":"../0.png"},{"path":"钢琴 - 副本 (8)","name":"菜鸟教程","url":"www.pixiv.com","img":"../0.png"},{"path":"钢琴 - 副本","name":"菜鸟教程","url":"www.pixiv.com","img":"../0.png"},{"path":"钢琴","name":"菜鸟教程","url":"www.pixiv.com","img":"../0.png"}]};
+    sites: [{
+            path: "钢琴 - 副本 (2)",
+            name: "菜鸟教程",
+            url: "../works/piano",
+            img: "../img/0.png",
+        },
+        {
+            path: "钢琴 - 副本 (3)",
+            name: "菜鸟教程",
+            url: "../works/piano/index.html",
+            img: "../img/0.png",
+        },
+        {
+            path: "钢琴 - 副本 (4)",
+            name: "菜鸟教程",
+            url: "../works/piano/index.html",
+            img: "../img/0.png",
+        },
+        {
+            path: "钢琴 - 副本 (5)",
+            name: "菜鸟教程",
+            url: "../works/piano/index.html",
+            img: "../img/0.png",
+        },
+        {
+            path: "钢琴 - 副本 (6)",
+            name: "菜鸟教程",
+            url: "../works/piano/index.html",
+            img: "../img/0.png",
+        },
+        {
+            path: "钢琴 - 副本 (7)",
+            name: "菜鸟教程",
+            url: "../works/piano/index.html",
+            img: "../img/0.png",
+        },
+        {
+            path: "钢琴 - 副本 (8)",
+            name: "菜鸟教程",
+            url: "../works/piano/index.html",
+            img: "../img/0.png",
+        },
+        {
+            path: "钢琴 - 副本",
+            name: "菜鸟教程",
+            url: "../works/piano/index.html",
+            img: "../img/0.png",
+        },
+        { path: "钢琴", name: "菜鸟教程", url: "www.pixiv.com", img: "../img/0.png" },
+    ],
+};
